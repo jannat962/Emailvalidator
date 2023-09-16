af9dc8013066eb975cda0334fe0696c3314ee480
Jannatul__khanam <102294457+jannat962@users.noreply.github.com>
2023-09-16 13:37:47 +0600

    first
    This project can be used to validate emails using the emailvalidation api

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("this is my script")
+
+let result =
+{
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "jannatulkhanam962",
+    "email": "jannatulkhanam962@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+}
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="loading.svg" alt="">`
+    let key = "ema_live_33mfnLZG1qZOv743d3asRGUUfEpcfHELY2AejncT"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
